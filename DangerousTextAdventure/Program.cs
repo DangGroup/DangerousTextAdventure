61d5dc97bb718937c2bb9f4fd91404b35929796d
SWiRaki <62663849+SWiRaki@users.noreply.github.com>
2020-04-09 20:32:07 +0200

    Projektdateien hinzufügen.

---
+++ DangerousTextAdventure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangerousTextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
